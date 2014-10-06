e47bf5eb0196162ed44df9b6bc2d4611cd55d19b
Jeroen Ransijn <Studio_04@jeroen.local>
2014-10-06 12:33:08 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# jquery.input-stepper.js
+> Lightweight input stepper for real world usage
+
+
+## Features
+- Works on input type="text"
+- Only supports positive integers 0 - Infinity
+- Set initial values in the markup or Javascript
+- Optionally configure min/max value with attributes
+- Supports keyboard interaction (increase/decrease by 10 by holding shift)
+- Enables number pad on mobile browsers
+
+## Install
+Download and include the javascript file.
+```html
+<script src="jquery.input-stepper.js"></script>
+```
+
+## Basic usage
+Check out `index.html` for all examples.
+
+```html
+<div class="input-stepper">
+	<button data-input-stepper-decrease>-</button>
+	<input type="text">
+	<button data-input-stepper-increase>+</button>
+</div>
+```
+
+Call the input stepper plugin on the desired selector
+
+```javascript
+$('.input-stepper').inputStepper();
+```
+
+
+## Advanced usage
+
+```html
+<div class="input-stepper-advanced">
+	<button class="decrease" data-decrease>-1</button>
+	<button class="decrease" data-decrease="5">-5</button>
+	<button class="decrease" data-decrease="10">-10</button>
+	<input type="text" value="50" min="25" max="75">
+	<button class="increase" data-increase>+1</button>
+	<button class="increase" data-increase="5">+5</button>
+	<button class="increase" data-increase="10">+10</button>
+</div>
+```
+
+Call the input stepper plugin on the desired selector
+
+```javascript
+$('.input-stepper-advanced').inputStepper({
+	selectorButtonIncrease: '.increase',
+	selectorButtonDecrease: '.decrease',
+	dataAttributeIncrease: 'increase',
+	dataAttributeDecrease: 'decrease'
+});
+```
+
+## Default settings
+The input stepper can be called with a number of options. The defaults of each option are listed below
+
+```javascript
+$('.input-stepper').inputStepper({
+
+	// Values in the markup always overwrite this settings
+	initialValue: null,
+	min: 0,
+	max: Infinity
+
+	// The selector for the input
+	selectorInput: 'input',
+
+	// Selectors for the increase/decrease buttons
+	selectorButtonIncrease: '[data-input-stepper-increase]',
+	selectorButtonDecrease: '[data-input-stepper-decrease]',
+
+	// Optional data attribtus to check for how much to increase/decrease
+	dataAttributeIncrease: 'input-stepper-increase',
+	dataAttributeDecrease: 'input-stepper-decrease',
+
+	// Classname added to increase/decrease buttons when they are disabled
+	// The disabled property is also set
+	classNameDisabled: 'is-disabled'
+});
+```
+
+## Custom events on the input element
+If your application listens change events on the input element, it can now also listen to increase/decrease events on the input element.
+
+```html
+<div class="input-stepper">
+	<button data-input-stepper-decrease>-</button>
+	<input id="some-input" type="text">
+	<button data-input-stepper-increase>+</button>
+</div>
+```
+
+```javascript
+$('#some-input').on('increase', function (e, amount) {
+	alert('increase with: ' + amount);
+});
+
+$('#some-input').on('decrease', function (e, amount) {
+	alert('decrease with: ' + amount);
+});
+```
+
+## Authors
+Jeroen Ransijn
---
+++ jquery.input-stepper.js
@@ -0,0 +1,205 @@
+// jquery.input-stepper.js
+// ------------------------------------------------------
+// Author: Jeroen Ransijn - Aan Zee
+;(function (root, $, undefined) {
+
+	var pluginName = "inputStepper";
+	var defaults = {
+		selectorButtonIncrease: '[data-input-stepper-increase]',
+		selectorButtonDecrease: '[data-input-stepper-decrease]',
+		dataAttributeIncrease: 'input-stepper-increase',
+		dataAttributeDecrease: 'input-stepper-decrease',
+		classNameDisabled: 'is-disabled',
+		selectorInput: 'input',
+		initialValue: null,
+		min: 0,
+		max: Infinity
+	};
+
+	// The actual plugin constructor
+	function Plugin( element, options ) {
+		this.element = element;
+
+		this.options = $.extend( {}, defaults, options) ;
+
+		this._defaults = defaults;
+		this._name = pluginName;
+
+		return this.init();
+	}
+
+	Plugin.prototype = {
+
+		init: function () {
+			var _this = this;
+
+			// Cache elements
+			this.$el = $(this.element);
+			this.$input = this.$el.find(this.options.selectorInput);
+			this.$decreaseButton = this.$el.find(this.options.selectorButtonDecrease);
+			this.$increaseButton = this.$el.find(this.options.selectorButtonIncrease);
+
+			this.min = this.$input.attr('min') || this.options.min;
+			this.max = this.$input.attr('max') || this.options.max;
+
+			this.initialValue = this.getValue() || this.options.initialValue || this.min;
+
+			this.setValue(this.initialValue);
+			this.checkState();
+
+			this.$input.attr('pattern', '[0-9]*');
+
+			// Bind events
+			this.$input.on('keydown', this.onKeyDown.bind(this) );
+
+			this.$input.on('change', this.onChange.bind(this) );
+
+			this.$increaseButton.on('click', function (e) {
+				e.preventDefault();
+				_this.increase( $(this).data( _this.options.dataAttributeIncrease ) || 1 );
+			});
+			this.$decreaseButton.on('click', function (e) {
+				e.preventDefault();
+				_this.decrease( $(this).data('input-stepper-decrease') || 1 );
+			});
+
+			return this;
+		},
+
+		onChange: function (e) {
+			if (this.getValue() === '') {
+				this.setValue( this.initialValue );
+			} else {
+				this.setValue( this.getValue() );
+			}
+
+			return this;
+		},
+
+		onKeyDown: function (e) {
+			// Inspired by:
+			// http://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
+
+			// Allow: backspace, delete, tab, escape, enter and .
+			var isEnterKey = e.keyCode == 13;
+
+			if (isEnterKey) {
+				this.setValue(this.getValue());
+				return e.preventDefault();
+			}
+
+			var isSomeKeys = $.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1;
+			// Allow: Ctrl+A
+			var isCntrlA = (e.keyCode == 65 && e.ctrlKey === true);
+			// Allow: home, end, left, right
+			var isHomeEndLeftRight = (e.keyCode >= 35 && e.keyCode <= 39);
+
+			var isUpKey = e.keyCode == 38;
+			var isDownKey = e.keyCode == 40;
+
+			if (isUpKey) this.increase( (e.shiftKey ? 10 : 1) );
+			if (isDownKey) this.decrease( (e.shiftKey ? 10 : 1) );
+
+			if (isSomeKeys || isCntrlA || isHomeEndLeftRight) return;
+
+			// Ensure that it is a number and stop the keypress
+			if ((e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) {
+				// Numbers 0-9
+				this.setValue(this.getValue());
+			}
+			if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) ) {
+
+				e.preventDefault();
+			}
+
+			return this;
+		},
+
+		getValue: function () {
+			return this.$input.val();
+		},
+
+		setValue: function (amount) {
+			amount = parseInt(amount, 10);
+
+			if (this.amount === parseInt(amount, 10) ) return;
+
+			if (amount !== '') {
+				this.amount = parseInt(amount, 10);
+			} else {
+				this.amount = this.initialValue;
+			}
+
+			if (this.amount >= this.max) {
+				this.amount = parseInt(this.max, 10);
+			}
+
+			if (this.amount <= this.min) {
+				this.amount = parseInt(this.min, 10);
+			}
+
+			this.$input.val(this.amount).trigger('change');
+			this.checkState();
+
+			return this;
+		},
+
+		increase: function (amount) {
+			if (this.$increaseButton.hasClass( this.options['classNameDisabled'] )) return;
+			var newAmount = parseInt(this.amount + (amount || 1), 10);
+
+			this.setValue(newAmount);
+			this.$input.trigger('increase', [amount]);
+
+			return this;
+		},
+
+		decrease: function (amount) {
+			if (this.$decreaseButton.hasClass( this.options['classNameDisabled'] )) return;
+			var newAmount = parseInt(this.amount - (amount || 1), 10);
+
+			this.setValue(newAmount);
+			this.$input.trigger('decrease', [amount]);
+
+			return this;
+		},
+
+		disableButton: function ($button) {
+			$button.addClass( this.options['classNameDisabled'] ).attr('disabled', true);
+			return this;
+		},
+
+		enableButton: function ($button) {
+			$button.removeClass( this.options['classNameDisabled'] ).attr('disabled', false);
+			return this;
+		},
+
+		checkState: function () {
+			if (this.amount <= this.min) {
+				this.disableButton( this.$decreaseButton );
+			} else {
+				this.enableButton( this.$decreaseButton );
+			}
+
+			if (this.amount >= this.max) {
+				this.disableButton( this.$increaseButton );
+			} else {
+				this.enableButton( this.$increaseButton );
+			}
+
+			return this;
+		}
+	};
+
+	// A really lightweight plugin wrapper around the constructor,
+	// preventing against multiple instantiations
+	$.fn[pluginName] = function ( options ) {
+		return this.each(function () {
+			if ( ! $.data(this, "plugin-" + pluginName)) {
+				$.data(this, "plugin-" + pluginName,
+					new Plugin( this, options ));
+				}
+		});
+	};
+
+})(window, jQuery);
